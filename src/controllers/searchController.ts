8cc85bbfe0585fc45a89dda3fc2850826562b662
Laura <analaura3rocha@gmail.com>
2025-02-14 12:46:04 -0300

    search finalizado

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,5 +1,14 @@
 import { Request, Response } from 'express'; 
+import { Movie } from '../data/dataMovies';
+
 
 export const search = (req: Request, res: Response) =>{
-    //
+    let query: string = req.query.q as string ;
+    
+    let list = Movie.getName(query);
+   
+
+    res.render('pages/index', { list, query })
+
+
 };
